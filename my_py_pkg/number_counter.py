254b0a91caf2456213f4b63467b1ae04ccbdf65e
Hutomo <hutomogundar@gmail.com>
2025-08-27 14:21:23 +0900

    move counter publishing to callback

--- my_py_pkg/number_counter.py
+++ my_py_pkg/number_counter.py
@@ -11,12 +11,12 @@ class NumberCounter(Node):
         self.subscriber_ = self.create_subscription(
             Int64, "number",self.callback_number_publisher, 10)
         self.publisher_ = self.create_publisher(Int64, "number_counter", 10)
-        self.timer_ = self.create_timer(0.5, self.publish_counter)
         self.get_logger().info("Number counter has been started")
     
     def callback_number_publisher(self, msg: Int64):
         self.get_logger().info(str(msg.data))
         self.counter_ += msg.data
+        self.publish_counter()
 
     def publish_counter(self):
         msg = Int64()
